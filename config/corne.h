7f9fd98f25bf9984a233898487417dfcf998e2b6
Nikolaj Sidorenco <Nikolaj176@hotmail.com>
2022-03-16 21:54:40 +0100

    Change keymap

--- config/corne.h
+++ config/corne.h
@@ -7,7 +7,7 @@
 #define SYM 3
 
 #define SK(KEY) &nk KEY KEY
-#define MO(LAYER) &mo_tap LAYER F24
+#define MO(LAYER, KEY) &mo_tap LAYER KEY
 
 // Keep sticky keys active for a long time so they effectively do not time out
 #define STICKY_KEY_TIMEOUT 60000
