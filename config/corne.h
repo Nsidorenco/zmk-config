af0d41926777e0e3c3a5ca97704ee1468f101e44
Nikolaj Sidorenco <Nikolaj176@hotmail.com>
2022-03-16 22:02:28 +0100

    Remove behaviours

--- config/corne.h
+++ config/corne.h
@@ -5,9 +5,3 @@
 #define NAV 1
 #define NUM 2
 #define SYM 3
-
-#define SK(KEY) &nk KEY KEY
-#define MO(LAYER, KEY) &mo_tap LAYER KEY
-
-// Keep sticky keys active for a long time so they effectively do not time out
-#define STICKY_KEY_TIMEOUT 60000
