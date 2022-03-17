efbbc370ee7155a7dc8acdb94ccb7f8749840da5
Nikolaj Sidorenco <Nikolaj176@hotmail.com>
2022-03-17 17:46:22 +0100

    Add nordic symbols

--- config/corne.h
+++ config/corne.h
@@ -11,3 +11,13 @@
 #define UC_PST LG(V)
 #define UC_UND LG(Z)
 #define UC_RDO LG(LS(Z))
+
+#define DK_SLSH LS(7)
+#define DK_DQT LS(2)
+#define DK_SQT PIPE
+#define DK_COLN LS(DOT)
+#define DK_MINS SLSH
+
+#define DK_AE COLON
+#define DK_AO SQT
+#define DK_AA LBKT
